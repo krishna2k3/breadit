782cf11067f38382b16dc01d9360c7fd178a818d
krishna2k3 <krishna101ravi@gmail.com>
2023-08-11 19:21:20 +0530

    minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,44 @@
-import { cn } from "@/lib/utils";
-import "@/styles/globals.css";
-import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import { Toaster } from "@/components/ui/Toaster";
-import Providers from "@/components/Providers";
+import Navbar from '@/components/Navbar'
+import { cn } from '@/lib/utils'
+import { Inter } from 'next/font/google'
+import Providers from '@/components/Providers'
+import { Toaster } from '@/components/ui/Toaster'
+
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Breadit",
-  description: "A Reddit clone built with Next.js and TypeScript.",
-};
-
-const inter = Inter({ subsets: ["latin"] });
+  title: 'Breadit',
+  description: 'A Reddit clone built with Next.js and TypeScript.',
+}
 
 export default function RootLayout({
   children,
   authModal,
 }: {
-  children: React.ReactNode;
-  authModal: React.ReactNode;
+  children: React.ReactNode
+  authModal: React.ReactNode
 }) {
   return (
     <html
-      lang="en"
+      lang='en'
       className={cn(
-        "bg-white text-slate-900 antialiased light",
+        'bg-white text-slate-900 antialiased light',
         inter.className
-      )}
-    >
-      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+      )}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
         <Providers>
-          {/* @ts-expect-error server component */}
+          {/* @ts-expect-error Server Component */}
           <Navbar />
           {authModal}
-          <div className="container max-w-7xl mx-auto h-full pt-12">
+
+          <div className='container max-w-7xl mx-auto h-full pt-12'>
             {children}
           </div>
         </Providers>
         <Toaster />
       </body>
     </html>
-  );
+  )
 }
