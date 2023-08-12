5aaf3b3e8f67c718b3507bd0cd846791560a3025
krishna2k3 <krishna101ravi@gmail.com>
2023-08-12 23:38:08 +0530

    displaying posts in the feed

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,19 +1,20 @@
-import MiniCreatePost from "@/components/MiniCreatePost";
-import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
-import { getAuthSession } from "@/lib/auth";
-import { db } from "@/lib/db";
-import { notFound } from "next/navigation";
+import MiniCreatePost from '@/components/MiniCreatePost'
+import PostFeed from '@/components/PostFeed'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
 
 interface PageProps {
   params: {
-    slug: string;
-  };
+    slug: string
+  }
 }
 
 const page = async ({ params }: PageProps) => {
-  const { slug } = params;
+  const { slug } = params
 
-  const session = await getAuthSession();
+  const session = await getAuthSession()
 
   const subreddit = await db.subreddit.findFirst({
     where: { name: slug },
@@ -25,20 +26,25 @@ const page = async ({ params }: PageProps) => {
           comments: true,
           subreddit: true,
         },
+        orderBy: {
+          createdAt: 'desc'
+        },
         take: INFINITE_SCROLLING_PAGINATION_RESULTS,
       },
     },
-  });
+  })
 
-  if (!subreddit) return notFound();
+  if (!subreddit) return notFound()
+
   return (
     <>
-      <h1 className="font-bold text-3xl md:text-4xl h-14">
+      <h1 className='font-bold text-3xl md:text-4xl h-14'>
         r/{subreddit.name}
       </h1>
       <MiniCreatePost session={session} />
+      <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
     </>
-  );
-};
+  )
+}
 
-export default page;
+export default page
