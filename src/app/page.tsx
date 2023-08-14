30f35dd48597467eb623e261c317ae4c9b877540
krishna2k3 <krishna101ravi@gmail.com>
2023-08-14 23:03:41 +0530

    custom feed for logged in and logged out users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import CustomFeed from '@/components/homepage/CustomFeed'
+import GeneralFeed from '@/components/homepage/GeneralFeed'
 import { buttonVariants } from '@/components/ui/Button'
+import { getAuthSession } from '@/lib/auth'
 import { Home as HomeIcon } from 'lucide-react'
 import Link from 'next/link'
 
@@ -6,11 +9,14 @@ export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
 export default async function Home() {
+  const session = await getAuthSession()
 
   return (
     <>
       <h1 className='font-bold text-3xl md:text-4xl'>Your feed</h1>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
 
         {/* subreddit info */}
         <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
