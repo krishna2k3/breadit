dc3a3b7f12d0a5eecdf01367ece5489583afe36a
krishna2k3 <krishna101ravi@gmail.com>
2023-08-19 11:27:59 +0530

    search bar implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import UserAccountNav from "./UserAccountNav";
+import SearchBar from "./SearchBar";
 
 const Navbar = async () => {
   const session = await getAuthSession();
@@ -18,6 +19,7 @@ const Navbar = async () => {
         </Link>
 
         {/* search bar */}
+        <SearchBar />
         {session?.user ? (
           <UserAccountNav user={session.user} />
         ) : (
